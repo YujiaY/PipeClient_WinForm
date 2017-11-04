b7ce836c8a6debcd892d4094a01fdc3600b99244
Unknown <jack.yujia.yuan@gmail.com>
2017-11-04 10:43:56 +0800

    PipeClient_WinForm
    And there is a PipeServer_WinForm

---
+++ PipeClient_WinForm/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PipeClient_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbReceive = new System.Windows.Forms.TextBox();
+            this.btnDisc = new System.Windows.Forms.Button();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(28, 127);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(87, 45);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Connect";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbReceive
+            // 
+            this.tbReceive.Location = new System.Drawing.Point(28, 204);
+            this.tbReceive.Multiline = true;
+            this.tbReceive.Name = "tbReceive";
+            this.tbReceive.Size = new System.Drawing.Size(231, 139);
+            this.tbReceive.TabIndex = 1;
+            // 
+            // btnDisc
+            // 
+            this.btnDisc.Location = new System.Drawing.Point(172, 127);
+            this.btnDisc.Name = "btnDisc";
+            this.btnDisc.Size = new System.Drawing.Size(87, 45);
+            this.btnDisc.TabIndex = 2;
+            this.btnDisc.Text = "Disconnect";
+            this.btnDisc.UseVisualStyleBackColor = true;
+            this.btnDisc.Click += new System.EventHandler(this.btnDisc_Click);
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.Location = new System.Drawing.Point(28, 13);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(231, 68);
+            this.lbInfo.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 367);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.btnDisc);
+            this.Controls.Add(this.tbReceive);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "PipeClient_WinForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbReceive;
+        private System.Windows.Forms.Button btnDisc;
+        private System.Windows.Forms.Label lbInfo;
+    }
+}
+
---
+++ PipeClient_WinForm/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Pipes;
+using System.IO;
+
+
+namespace PipeClient_WinForm
+{
+    public partial class Form1 : Form
+    {
+        NamedPipeClientStream pipeClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lbInfo.Text = "\nClick Connect to set up connection";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Connect to the pipe or wait until the pipe is available.
+            //  
+            // NamedPipeClientStream pipeClient1 = new NamedPipeClientStream("123");
+            pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.In);
+            //if (!pipeClient.IsConnected)
+
+            pipeClient.Connect();
+
+            lbInfo.Text += "\nConnected to pipe.";
+            lbInfo.Text += $"There are currently" + pipeClient.NumberOfServerInstances.ToString()
+                + "pipe server instances open.";
+            lbInfo.Text += "\nSwitch over to the server program to input text there.";
+
+
+            // Now read the text stream that comes in via the named pipe
+            using (StreamReader sr = new StreamReader(pipeClient))
+            {
+                // Display the text to the console
+                string temp;
+                if ((temp = sr.ReadLine()) != null)
+                {
+                    tbReceive.Text = "Received from server:";
+                    tbReceive.Text += temp;
+                }
+            }
+
+
+
+
+
+            
+
+
+
+        }
+
+        private void btnDisc_Click(object sender, EventArgs e)
+        {
+            if (pipeClient != null)
+            {
+                pipeClient.Close();
+            }
+
+            lbInfo.Text = "Connection Closed";
+        }
+
+
+    }
+}
